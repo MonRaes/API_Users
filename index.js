690ad52d972d060d0ddef907e5fa0395dc99c402
Mon_Raes <130837443+MonRaes@users.noreply.github.com>
2024-09-02 23:40:24 +0000

    update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+require('dotenv').config()
+
 // Configuração do MySQL
   const connection = mysql.createConnection({
     host: 'localhost',
